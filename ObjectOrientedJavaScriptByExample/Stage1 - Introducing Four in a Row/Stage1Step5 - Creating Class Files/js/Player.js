03ec6113c5c77a720e95fc2e7548551030e0cdcf
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-04-18 14:17:25 -0700

    partial project

--- ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
+++ ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
@@ -20,4 +20,22 @@ class Player {
     }
     return tokens; 
   };
+ /**
+  * Gets all tokens that haven't been dropped.
+  * @return {array} Array of unused tokens.
+  */
+  get unusedTokens(){
+    return this.tokens.filter(token => !token.dropped);
+  }
+ 
+/**
+ * Get the active token by returning the first token in the array of unused tokens.
+ * @return {Object} First token object of the array of unused tokens.
+ */
+ get activeToken(){
+     return this.unusedTokens[0];
+ }
+
 }
+
+
--- ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Token.js
+++ ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Token.js
@@ -5,4 +5,16 @@ class Token {
     this.dropped = false;
  }
 
+ drawHTMLToken(){
+    const token = document.createElement('div');
+    document.getElementById('game-board-underlay').appendChild(token);
+    div.setAttribute("id", this.id);
+    div.setAttribute("class", "token");
+    div.style.backgroundColor = this.owner.color; 
+ }
+
+ get htmlToken(){
+    return document.getElementbyId(this.id);
+ }
+
 }
